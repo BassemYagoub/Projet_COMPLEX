9fbb793f32fff578ea15d1f4a2aebe1d33220487
ohouens <rohouens@yahoo.fr>
2019-10-09 11:06:20 +0200

    try,finally enlevé car cause erreur + ajout plt.show pour montrer graphe

--- projet_ohouens_yagoub.py
+++ projet_ohouens_yagoub.py
@@ -5,67 +5,52 @@ Created on Mon Oct  7 11:25:22 2019
 """
 
 import networkx as nx
-#import matplotlib.pyplot as plt
-
-"""import numpy as np
-class Graphe():
-
-    def __init__(self, nbSommets, nbArretes):
-        self.nbS = nbSommets;
-        self.nbA = nbArretes;
-        self.listeAdj = np.array()
-        
-    def estimClass(self, attrs):
-        return 1.0
-"""
+import matplotlib.pyplot as plt
 
 def importGrapheFromTxt(file):
     """
-    params : 
+    params :
         file (string) : nom du fichier texte
     return : nx.Graph() (graphe) avec les sommets et arêtes lus dans le fichier.
     """
-    try:
-        f = open(file, "r")
-        sommets = [] #liste des sommets
-        aretes  = [] #liste des couples (sommet, sommet) (arêtes)
+    f = open(file, "r")
+    sommets = [] #liste des sommets
+    aretes  = [] #liste des couples (sommet, sommet) (arêtes)
+    ligne = f.readline()
+    checkpoint = "Sommets" #checkpoint initial
+
+    while(ligne):
+        ligne = ligne.replace("\n", "")
+        ligne = ligne.replace("\t", "")
+        if(checkpoint != "Aretes"):
+            ligne = ligne.replace(" ", "")
+
+        if(not str.isnumeric(ligne) and checkpoint != "Aretes"):
+            checkpoint = ligne
+
+        if(checkpoint == "Sommets"):
+            if(str.isnumeric(ligne)):
+                sommets.append(int(ligne))
+
+        #on a passé "Aretes" mais on n'ajoute pas sa ligne
+        elif(checkpoint == "Aretes" and ligne !="Aretes"):
+            sommet1, sommet2 = ligne.split() #les s1 et s2 composent l'arete
+            aretes.append((int(sommet1), int(sommet2))) #liste string devient couple d'int
+
         ligne = f.readline()
-        checkpoint = "Sommets" #checkpoint initial
-        
-        while(ligne):
-            ligne = ligne.replace("\n", "")
-            ligne = ligne.replace("\t", "")
-            if(checkpoint != "Aretes"):
-                ligne = ligne.replace(" ", "")
-                
-            if(not str.isnumeric(ligne) and checkpoint != "Aretes"):
-                checkpoint = ligne
-                
-            if(checkpoint == "Sommets"):
-                if(str.isnumeric(ligne)):
-                    sommets.append(int(ligne))
-                    
-            #on a passé "Aretes" mais on n'ajoute pas sa ligne
-            elif(checkpoint == "Aretes" and ligne !="Aretes"):
-                sommet1, sommet2 = ligne.split() #les s1 et s2 composent l'arete
-                aretes.append((int(sommet1), int(sommet2))) #liste string devient couple d'int
-                    
-            ligne = f.readline()
 
         #creation du graphe à retourner
         graphe = nx.Graph()
         graphe.add_nodes_from(sommets)
         graphe.add_edges_from(aretes)
         return graphe
-    
-    finally:
-        f.close()
 
 def dessine(G):
     """
     dessine le graphe G
     """
     nx.draw(G, with_labels=True)
+    plt.show()
 
 #2.1.1, 2.1.2
 def sousGraphe(G, v):
@@ -73,7 +58,7 @@ def sousGraphe(G, v):
     params:
         G: nx.Graph(), le graphe initial
         v: le(s) sommet(s) à retirer de G
-    
+
     return : un graphe graphe G2 obtenu à partir de G en supprimant le(s) sommet(s) v (ou de v)
     """
     G2 = nx.Graph(G)
@@ -109,8 +94,12 @@ def degreMax(G):
 #----------------------------------------------------------
 
 graphe = importGrapheFromTxt("exempleinstance.txt")
+dessine(graphe)#ne m'affiche rien
 
-graphe2 = sousGraphe(graphe, [0,1,2,8])
+grapheP = sousGraphe(graphe, [0,1,2,8])
+dessine(grapheP)#ne m'affiche rien
+
 graphe.add_edges_from([(1, 3), (1, 2)])
-dessine(graphe)
+dessine(graphe)#m'affiche un chemin 2-1-3
+
 print(degreMax(graphe))
